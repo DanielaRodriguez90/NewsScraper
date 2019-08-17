147fbd2e2eb7bdd0d2e07fcf3dc9aef46e124d28
daniela rodriguez <daniela_rodriguez1990@outlook.com>
2019-08-17 18:10:04 -0400

    finished/need to deploy

---
+++ server.js
@@ -0,0 +1,46 @@
+//dependencies
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+//initialize Express app
+const express = require("express");
+const app = express();
+
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+//Require set up handlebars
+const exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
+
+const routes = require("./controller/controller.js");
+app.use("/", routes);
+//Create localhost port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Listening on PORT ${PORT}`);
+});
